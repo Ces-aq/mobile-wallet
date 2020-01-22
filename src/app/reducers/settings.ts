c210ca0d8fd3a18066bfdad936007f0048fe6a2f
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-01-22 07:26:38 -0300

    feat(settings-redux): Add settings reducer

---
+++ src/app/reducers/settings.ts
@@ -0,0 +1,19 @@
+import { ActionTypes } from "../actions/settings";
+import { ActionModel } from "../models/action";
+import { UserSettings } from "../models/settings";
+
+export const settings = new UserSettings();
+
+export function settingsReducer(state = settings, action: ActionModel) {
+	const { type, payload } = action;
+	switch (type) {
+		case ActionTypes.SET: {
+			state[payload.field] = payload.value;
+
+			console.log(state);
+			return state;
+		}
+		default:
+			return state;
+	}
+}
